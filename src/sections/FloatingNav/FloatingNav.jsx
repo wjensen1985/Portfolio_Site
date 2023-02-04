8dd3582ca3ac99939132d307dc68289e6d7690cb
wjensen1985 <101837132+wjensen1985@users.noreply.github.com>
2023-02-04 14:53:24 -0500

    fixed floating nav, now highlights menu item based on scroll position so that the section currently being viewed is highlighted. removed scrollposition component as no longer used. also removed on click from floating nav buttons as highlight change is now done based on scroll position

--- src/sections/FloatingNav/FloatingNav.jsx
+++ src/sections/FloatingNav/FloatingNav.jsx
@@ -1,19 +1,52 @@
 import React from 'react'
 import './floatingNav.css'
 import data from './data'
-import { useState } from 'react'
-import { useScrollPosition } from '../../components/useScrollPosition'
+import { useState, useEffect } from 'react'
+// import { useScrollPosition } from '../../components/useScrollPosition'
 
 const FloatingNav = () => {
-  const [active, setActive] = useState('#');
-  const scrollPosition = useScrollPosition();
-  console.log(scrollPosition);
+  const [active, setActive] = useState('');
+  // const scrollPosition = useScrollPosition();
+
+  useEffect(() => {
+    const updateActive = () => {
+      const sectionAll = document.querySelectorAll('section[id]');
+      const scrollPosition = window.pageYOffset;
+      // console.log(sectionAll[0].offsetTop);
+      // console.log(scrollPosition);
+      const headerPosition = sectionAll[0].offsetTop;
+      // console.log(headerPosition);
+
+      sectionAll.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 200;
+        const sectionId = current.getAttribute('id');
+        
+        if (scrollPosition < (headerPosition - 201)){
+          // console.log('home');
+          setActive('#');
+        }
+
+        if (scrollPosition > sectionTop && scrollPosition < sectionTop + sectionHeight){
+          setActive(`#${sectionId}`);
+          // console.log(sectionId);
+        }
+      });
+    };
+
+    window.addEventListener('scroll', updateActive);
+
+    updateActive();
+
+    return () => window.removeEventListener('scroll', updateActive);
+
+  }, []);
 
   return (
     <>
       <ul id='floating_nav'>
         {
-          data.map(item=> <li key={item.id}><a onClick = {() => setActive(`${item.link}`)} className = {active === `${item.link}` ? 'active' : ""} href={item.link}>{item.title}</a></li>)
+          data.map(item=> <li key={item.id}><a className = {active === `${item.link}` ? 'active' : ""} href={item.link}>{item.title}</a></li>)
         }
       </ul> 
     </>  
@@ -21,3 +54,6 @@ const FloatingNav = () => {
 }
 
 export default FloatingNav
+
+
+// {/* onClick = {() => setActive(`${item.link}`)}  */}
