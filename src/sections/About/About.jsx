5775c6352015a046b23b1099294809d881a470dc
wjensen1985 <101837132+wjensen1985@users.noreply.github.com>
2023-02-19 17:27:43 -0500

    finsihed about section

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './About.css'
+import {FaDownload} from 'react-icons/fa'
+import Resume from '../../assets/CV_ex.pdf'
 
 const About = () => {
   return (
-    <section id="about"><h2>About</h2>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p>
-    <a className="btn primary" target='_blank' rel="noopener noreferrer" href="/#">Download Resume</a>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p></section>
+    <section id="about"><h2>About Me</h2>
+      <div className="about-container">
+        <h3>My Mission</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p>
+        <h3>My Experiences</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit praesentium tenetur inventore ratione, modi, et quae sed totam illum quidem molestias veniam aperiam corporis expedita beatae natus harum, nulla doloremque!</p>
+        <a className="btn primary" href={Resume} download >Download CV <FaDownload/></a>
+      </div>
+    </section>
   )
 }
 
